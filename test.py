c8649f54ee2b565f28bbe77b8a14fbfdef32c0f0
Zeus6768 <akaps6768@gmail.com>
2021-10-14 22:22:12 +0900

    211014 update 7th week

--- test.py
+++ test.py
@@ -1,29 +1,36 @@
-def argmax(array, axis=None):
-    col = len(array[0])
-    result_array = []
-    if axis is None: 
-        for a in array:
-            result_array += a
-        return result_array.index(max(result_array))
-    elif axis == 0:
-        for i in range(col):
-            temp = []
-            for a in array:
-                temp.append(a[i])
-            result_array.append(temp.index(max(temp)))
-        return result_array
-    elif axis == 1:
-        for a in array:
-            result_array.append(a.index(max(a)))
-        return result_array
-    else:
-        return "error"
+import numpy as np
 
-import random
+a = np.arange(8).reshape(2, 4)
+b = np.arange(8).reshape(4, 2)
 
-a = [[random.randrange(1, 10) for _ in range(3)] for i in range(3)]
-print(a)
+#print(np.dot(a, b))
 
-print(argmax(a))
-print(argmax(a, axis=0))
-print(argmax(a, axis=1))
+# tensors
+x = np.arange(24).reshape(2, 3, 4)
+#print(x)
+
+c = np.dot(a, b)
+#print(c)
+
+d = np.einsum('ik, kj -> ij', a, b)
+#print(d)
+
+aa = np.arange(9).reshape(3, 3)
+#print(aa)
+#e = np.einsum('ii->i', aa)
+e = np.einsum('ij->i', aa)
+f = np.einsum('ij->ji', aa)     # aa.T == F
+#print(e)
+#print(f)
+
+a2 = np.arange(60.).reshape(3, 4, 5)
+b2 = np.arange(24.).reshape(4, 3, 2)
+c2 = np.einsum('ijk, jil -> kl', a2, b2)
+# print(a2)
+# print(b2)
+# print(c2)
+
+# Norm
+u = np.array([3, -4])
+print(np.linalg.norm(u))
+print(np.abs(u).sum())
